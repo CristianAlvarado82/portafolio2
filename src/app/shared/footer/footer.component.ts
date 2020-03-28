90f8737b9e73baa2f553c3eaed6813cb73c1b45a
CristianAlvarado82 <codigoid@hotmail.com>
2020-03-28 01:53:09 -0400

    cargado inicial

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  gestion: number = new Date().getFullYear();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
