9f0d7260f0c424311e14ea08d1ad3ee7d542b57e
CristianAlvarado82 <codigoid@hotmail.com>
2020-04-02 15:54:38 -0400

    fin seccion 6

---
+++ src/app/service/productos.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando=true;
+  productos: ProductoInterface[] =[];
+
+  constructor(private http:HttpClient) { 
+
+    this.cargarProductos();
+
+  }
+
+  private cargarProductos(){
+
+    this.http.get('https://angular-practicas.firebaseio.com/productos_idx.json')
+    .subscribe((resp:ProductoInterface[])=>{
+      console.log(resp);
+      this.productos = resp;
+      //console.log(this.productos);
+      setTimeout(() => {
+        this.cargando = false;
+      }, 2000);
+    })
+  }
+}
